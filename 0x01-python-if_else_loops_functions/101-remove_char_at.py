f54c97f3ffd8a5d4ba2442ec47c96565ed2bb876
ck-ngetich <kiprongetich3@gmail.com>
2023-11-29 08:53:46 -0800

    withdrawal from position

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-# Author -Hardware-and-electronics-prototype-org
 def remove_char_at(str, n):
     if n < 0:
         return (str)
-    return (str[:n] + str[n+1:]i)
+    return (str[:n] + str[n+1:])
