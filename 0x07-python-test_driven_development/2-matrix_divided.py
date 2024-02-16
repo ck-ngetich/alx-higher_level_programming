169c0960e1c0349ebf0b8d47fc556fe82d4894a2
ck-ngetich <kiprongetich3@gmail.com>
2024-02-16 13:47:08 -0800

    matrix division

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-matrix_divided divides the given matrix by the parameter and returns the divided one
+Module matrix_divided
+Divides the given matrix by the parameter and returns the divided one
 """
 def matrix_divided(matrix, div):
     """
@@ -8,23 +9,33 @@ def matrix_divided(matrix, div):
     Checks if each list of the matrices have the same size
     checks if "div" is an int/float or is 0
     """
-    matrix_res= []
 
-    if not isinstance(div, (int, float)):
+
+    if not isinstance(matrix, list) or len(matrix) == 0 or not matrix[0]:
+        raise TypeError("matrix must be a matrix (list of lists) " +
+                        "of integers/floats")
+
+    for row in matrix:
+        if len(row) == 0:
+            raise TypeError("matrix must be a matrix (list of lists) " +
+                            "of integers/floats")
+        for x in row:
+            if type(x) is not int and type(x) is not float:
+                raise TypeError("matrix must be a matrix (list of lists) " +
+                                "of integers/floats")
+
+    len_rows = []
+    for row in matrix:
+        len_rows.append(len(row))
+    if not all(elem == len_rows[0] for elem in len_rows):
+        raise TypeError("Each row of the matrix must have the same size")
+
+    if type(div) is not int and type(div) is not float:
         raise TypeError("div must be a number")
 
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-    for lists in matrix:
-        if len(lists) != len(matrix[0]):
-            raise TypeError("Each row of the matrix must have the same size")
-        inner_list = []
-        for items in lists:
-            if not isinstance(items, (int, float)):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-            else:
-                inner_list.append(round(items / div, 2))
-        matrix_res.append(inner_list)
+    new_matrix = [[round(x / div, 2) for x in row] for row in matrix]
 
-    return matrix_res
+    return new_matrix
