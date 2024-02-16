c5cc3a6b1613fd581b3d61ce74d8c6ee4f16b9e8
ck-ngetich <kiprongetich3@gmail.com>
2024-02-16 14:21:17 -0800

    matrix multiplication

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+
+"""Module matrix_mul
+Multiplies two matrices .
+"""
+
 def matrix_mul(m_a, m_b):
+    """Give  matrix resulting of
+    the multiplication of m_a and m_b.
+    """
     if type(m_a) is not list:
         raise TypeError("m_a must be a list")
     x = len(m_a)
