d1fe9e9e2bd8e5f0a39983cb56159ec4822ea8ac
ck-ngetich <kiprongetich3@gmail.com>
2024-02-16 14:26:09 -0800

    Lazy matrix multiplicatioon

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+
+"""Module lazy_matrix_mul
+Multiply Matric using NumPy.
+"""
 import numpy
-
-
 def lazy_matrix_mul(m_a, m_b):
+    """Multiplies m_a and m_b using buitin function
+    matmul, and returns the result .
+    """
+    return numpy.matmul(m_a, m_b)
     return numpy.matmul(m_a, m_b)
