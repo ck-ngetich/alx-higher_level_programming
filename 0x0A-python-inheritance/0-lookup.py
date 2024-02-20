a8d0f27b72289c8603b6325f454b8f52e57ed347
ck-ngetich <kiprongetich3@gmail.com>
2024-02-20 04:33:56 -0800

    Lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""
+Define lookup function
+"""
+
+def lookup(obj):
+    """Function that returns the list of available attributes and methods of an object"""
+    return dir(obj)
