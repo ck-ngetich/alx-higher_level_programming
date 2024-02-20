8f7f4425b8bd340d4b5d11a9631f51c952fba7dc
ck-ngetich <kiprongetich3@gmail.com>
2024-02-20 11:27:10 -0800

    To JSON string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Module that contains to_json_string function
+"""
+
+import json
+
+
+def to_json_string(my_obj):
+    """Function that returns the JSON representation of an object"""
+    return json.dumps(my_obj)
