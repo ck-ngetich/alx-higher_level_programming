1246cf85871998617b28eda377b61516b0e627e2
ck-ngetich <kiprongetich3@gmail.com>
2024-02-21 11:13:57 -0800

    Only sub class of

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Module containing  the inherits_from function
+"""
+
+def inherits_from(obj, a_class):
+    """Function that returns True if obj is an instance of that
+    inherited directly or indirectly of a_class, otherwise False
+    """
+    return(issubclass(type(obj), a_class) and type(obj) != a_class)
