7280314281708f942842e1913c0394ef24b31be3
ck-ngetich <kiprongetich3@gmail.com>
2024-02-29 04:38:40 -0800

    file one pycodestyle validation

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,7 +4,6 @@ Module  that contains the read_file function
 """
 
 
-
 def read_file(filename=""):
     """Function that reads a text file(UTF8) and prints it to stdout"""
     with open(filename, "r", encoding="utf-8") as f:
