13294a6ab6aace3eb9c96f850241075b845085c2
ck-ngetich <kiprongetich3@gmail.com>
2024-03-11 04:18:16 -0700

    pycodestyling

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -3,6 +3,9 @@
 Define lookup function
 """
 
+
 def lookup(obj):
-    """Function that returns the list of available attributes and methods of an object"""
+    """Function that returns the list of available attributes
+    and methods of an object
+    """
     return dir(obj)
