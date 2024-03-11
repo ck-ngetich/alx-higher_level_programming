225f91d569de19e5d0576fecfe566c8b939ecaa6
ck-ngetich <kiprongetich3@gmail.com>
2024-03-11 04:31:23 -0700

    styling code

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -5,7 +5,7 @@ Module containing  the inherits_from function
 
 
 def inherits_from(obj, a_class):
-    """Function that returns True if obj is an instance of that
+    """ Function that returns True if obj is an instance of that
     inherited directly or indirectly of a_class, otherwise False
     """
     return(issubclass(type(obj), a_class) and type(obj) != a_class)
