b75309d68e4be34eb248af27fc87659dec440c19
ck-ngetich <kiprongetich3@gmail.com>
2024-03-26 14:57:45 -0700

    create database

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,6 +1,3 @@
-/*
-creates the database hbtn_0c_0
-should not fail if already exists
-*/
+-- Creates the database hbtn_0c_0
+-- Should not fail if already exist
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
-
