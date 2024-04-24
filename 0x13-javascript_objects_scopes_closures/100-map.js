834260606ab59b5d29058c256952be14e0c58af6
ck-ngetich <kiprongetich3@gmail.com>
2024-04-24 14:52:21 -0700

    factor index

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 const list = require('./100-data.js').list;
 console.log(list);
-console.log(list.map((init_list, index) => init_list * index));
+console.log(list.map((initList, index) => initList * index));
