360071a3cb23d59753d6e175bee88cb918c45509
ck-ngetich <kiprongetich3@gmail.com>
2024-04-23 18:36:42 -0700

    Square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+if (process.argv[2] === undefined || isNaN(process.argv[2])) {
+  console.log('Missing size');
+} else {
+  const n = Number(process.argv[2]);
+  let x = 0;
+  while (x < n) {
+    console.log('X'.repeat(n));
+    x++;
+  }
+}
