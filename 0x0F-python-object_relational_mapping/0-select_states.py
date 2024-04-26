25ae18003dd9baadb43eee73859b1f3e2462c34a
ck-ngetich <kiprongetich3@gmail.com>
2024-04-26 10:16:01 -0700

    Get all states of usa

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,7 +6,7 @@ from sys import argv
 
 if __name__ == "__main__":
     connector = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-            passwd=argv[2], db=argv[3], charset="utf8")
+                                passwd=argv[2], db=argv[3], charset="utf8")
     cursor = connector.cursor()
     cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
     rows = cursor.fetchall()
