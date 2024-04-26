219c50e3af58e0ed838c735009c920c331c2e25e
ck-ngetich <kiprongetich3@gmail.com>
2024-04-26 15:16:45 -0700

    First state model

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Script that connect to a MySQL server running on localhost at port 3306"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+from model_city import City
+
+Base = declarative_base()
+
+
+class State(Base):
+    """This is a class defining states table"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False,
+                autoincrement=True, unique=True)
+    n = Column(String(128), nullable=False)
