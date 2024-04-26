5c7821621798c18451e4605886e350422d36993f
ck-ngetich <kiprongetich3@gmail.com>
2024-04-26 10:20:23 -0700

    Filter states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Script that lists all states with a name starting with N (upper N) from the database"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    connect = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                              passwd=argv[2], db=argv[3], charset="utf8")
+    cursor = connect.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+    rows = cursor.fetchall()
+    for row in rows:
+        if row[1].startswith("N"):
+            print(row)
+    cursor.close()
+    connect.close()
